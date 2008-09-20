fa95019712b5cf4726bc301e708f902329823ac6
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2008-09-20 15:10:08 +0000

    Module language - Przerobienie systemu templatów i drobne aktualizacje w trakcie

--- trunk/modules/language/models/Language.php
+++ trunk/modules/language/models/Language.php
@@ -4,7 +4,6 @@ require_once 'page/models/Page.php';
 require_once 'news/models/News.php';
 require_once 'calendar/models/CalendarContent.php';
 require_once 'gallery/models/GalleryDescription.php';
-require_once 'gallery/models/GalleryImageDescription.php';
 
 require_once 'Zend/Db/Table/Abstract.php';
 class Language extends Zend_Db_Table_Abstract {
@@ -14,7 +13,6 @@ class Language extends Zend_Db_Table_Abstract {
 		'Page',
 		'News',
 		'CalendarContent',
-		'GalleryDescription',
-		'GalleryImageDescription'
+		'GalleryDescription'
 	);
 }
