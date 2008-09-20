49c02c29eddeffb07c4da28217617c69e34d2e41
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2008-09-20 15:11:26 +0000

    Module admin - Przerobienie systemu templatów i drobne aktualizacje w trakcie

--- trunk/modules/admin/controllers/IndexController.php
+++ trunk/modules/admin/controllers/IndexController.php
@@ -1,10 +1,8 @@
 <?php
+require_once 'KontorX/Controller/Action.php';
 class Admin_IndexController extends KontorX_Controller_Action {
-	public function init() {
-		$this->_initLayout();
-		$this->view->messages = $this->_helper->flashMessenger->getMessages();
-	}
+
+	public $skin = array('layout' => 'admin');
 	
-	public function indexAction() {
-	}
+	public function indexAction() {}
 }
