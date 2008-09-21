0f972e25925808aadc17a5d7f2d3e93abc9ce4a8
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2008-09-21 13:28:56 +0000

    Module system - zarządzanie konfiguracją systemu i modułami! każdy najdrobniejszy szczegół :D

--- modules/system/controllers/AdminController.php
+++ modules/system/controllers/AdminController.php
@@ -7,13 +7,8 @@ require_once 'KontorX/Controller/Action.php';
  * @author widmogrod
  */
 class System_AdminController extends KontorX_Controller_Action {
-    public function init() {
-        $this->_helper->system->template('admin');
-    }
-
+	
     public function indexAction() {
-        $config = $this->_helper->loader->config('test.ini');
-        $form = new KontorX_Form_Config($config);
-        $this->view->form = $form;
+    	$this->_forward('index','settings');
     }
 }
