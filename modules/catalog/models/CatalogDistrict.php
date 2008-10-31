8d0d018a83698a46501d6ff1337cc2ce4b997242
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2008-10-31 10:31:15 +0000

    Update catalog routers

--- modules/catalog/models/CatalogDistrict.php
+++ modules/catalog/models/CatalogDistrict.php
@@ -13,22 +13,6 @@ class CatalogDistrict extends KontorX_Db_Table_Tree_Abstract {
 	protected $_dependentTables = array(
 		'Catalog'
 	);
-	
-//	protected $_referenceMap    = array(
-//        'Language' => array(
-//            'columns'           => 'language_url',
-//            'refTableClass'     => 'Language',
-//            'refColumns'        => 'url',
-//			'refColumnsAsName'  => 'name',
-//			'onDelete'			=> self::CASCADE
-//        ),
-//        'User' => array(
-//            'columns'           => 'user_id',
-//            'refTableClass'     => 'User',
-//            'refColumns'        => 'id',
-//			'refColumnsAsName'  => 'username'
-//        )
-//    );
 
     /**
      * Wyłowienie widocznego publicznie rekordu
@@ -48,54 +32,7 @@ class CatalogDistrict extends KontorX_Db_Table_Tree_Abstract {
 
 		return $this->fetchRow($select);
     }
-
-//    /**
-//     * Przygotowuje ogólne zapytanie @see Zend_Db_Select
-//     * 
-//     * Przygotowuje ogólne zapytanie dla interfejsu
-//     * publicznego
-//     *
-//     * @param string $language
-//     * @param Zend_Db_Select $select
-//     * @return Zend_Db_Select
-//     */
-//    public function selectPublic($language, Zend_Db_Select $select = null) {
-//    	$select = (null === $select)
-//    		? $this->select()
-//    		: $select;
-//
-//    	$select
-//			->where('language_url = ?', $language)
-//			->where('publicated = 1');
-//
-//		return $select;
-//    }
-    
-//    /**
-//     * Nazwa kolumny przechowującej rodzaj widoczności rekordu
-//     *
-//     * @var string
-//     */
-//    protected $_columnForSpecialCredentials = 'visible';
 }
 
 require_once 'KontorX/Db/Table/Tree/Row.php';
-class CatalogDistrict_Row extends KontorX_Db_Table_Tree_Row {
-	public function findDependentBlocksRowset() {
-		$table = $this->getTable();
-		$db = $table->getAdapter();
-
-		require_once 'Zend/Db/Select.php';
-		$select = new Zend_Db_Select($db);
-		
-		$select = $select
-			->from(array('pb' => 'page_block'))
-			->join(array('pbs' => 'page_blocks'),'pb.block_id = pbs.id',array('block_name' => 'pbs.name'))
-			->where('pb.page_id = ?', $this->id);
-
-			
-		$stmt = $select->query();
-		$result = $stmt->fetchAll(Zend_Db::FETCH_CLASS);
-		return $result;
-	}
-}
+class CatalogDistrict_Row extends KontorX_Db_Table_Tree_Row {}
