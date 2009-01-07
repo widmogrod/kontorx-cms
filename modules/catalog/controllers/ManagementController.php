f4746da88969ecda7caddde92b85770dc127e09a
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2009-01-07 22:34:00 +0000

    Update

---
+++ modules/catalog/controllers/ManagementController.php
@@ -0,0 +1,119 @@
+<?php
+require_once 'KontorX/Controller/Action.php';
+class Catalog_ManagementController extends KontorX_Controller_Action {
+
+	public $skin = array(
+		'layout' => 'administration'
+	);
+	
+	public $ajaxable = array(
+		'service' => array('html')
+	);
+
+	public function init() {
+		parent::init();
+
+		$ajaxContext = $this->_helper->getHelper('AjaxContext');
+		$ajaxContext->initContext();
+	}
+	
+	public function indexAction(){
+		require_once 'catalog/models/Management.php';
+		$manage = new Management();
+
+		$rowset = $manage->findCatalogRowsetForUser($this->getRequest());
+		$this->view->rowset = $rowset;
+	}
+	
+	public function editAction() {
+		require_once 'catalog/models/Management.php';
+		$manage = new Management();
+		
+		$rq = $this->getRequest();
+		
+		$row = $manage->findCatalogRowForUser($this->_getParam('id'), $rq);
+		$this->view->row = $row;
+
+		if (null === $row) {
+			 $this->_helper->viewRenderer->render('edit.error');
+			 return;
+		}
+
+		$form = $this->_getEditForm($row);
+		$this->view->form = $form;
+		
+		if (!$rq->isPost()) {
+			$form->setDefaults($row->toArray());
+			return;
+		}
+
+		if (!$form->isValid($rq->getPost())) {
+			return;
+		}
+
+		try {
+			$data = $rq->getPost();
+
+			if (isset($data['user_id'])) {
+				unset($data['user_id']);
+			}
+			
+			$row->setFromArray($data);
+			$row->save();
+			
+			$message = "Wizytówka została zedytowana";
+			$this->_helper->flashMessenger($message);
+			
+			$this->_helper->redirector->goToUrlAndExit(
+				$this->_helper->url->url(array())
+			);
+		} catch(Zend_Db_Table_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString());
+
+			$message = "Wizytówka nie została zedytowana, proszę spróbować jeszcze raz";
+			$this->_helper->flashMessenger($message);
+		}
+	}
+
+	/**
+	 * @return Catalog_Form_ManagementCatalogEditForm
+	 */
+	private function _getEditForm(Zend_Db_Table_Row_Abstract $row) {
+		require_once 'Catalog/Form/ManagementCatalogEditForm.php';
+		$form = new Catalog_Form_ManagementCatalogEditForm($row->getTable());
+		
+		return $form;
+	}
+
+	public function serviceAction() {
+		require_once 'catalog/models/Management.php';
+		$manage = new Management();
+		
+		$id = $this->_getParam('id');
+		$rq = $this->getRequest();
+
+		// Czy rekord nalerzy do uzytkownika!?
+		if (null === ($row = $manage->findCatalogRowForUser($id, $rq))) {
+			$this->_helper->viewRenderer->render('edit.error');
+			 return;
+		}
+
+		$this->view->row = $row;
+		$this->view->rowset = $manage->findServicesRowsetForCatalogId($id);
+		
+		if ($rq->isPost()) {
+			if ($manage->saveServicesCost($id, $rq)) {
+				$message = "Usługi zostały zapisane";
+				$this->_helper->flashMessenger($message);
+
+				$this->_helper->redirector->goToUrlAndExit(
+					$this->_helper->url->url(array())
+				);
+			} else {
+				$message = "Usługi nie zostały zapisane";
+				$this->_helper->flashMessenger($message);
+			}
+		}
+	}
+}
---
+++ modules/catalog/models/Management.php
@@ -0,0 +1,147 @@
+<?php
+class Management {
+
+	/**
+	 * @param $rq
+	 * @return Zend_Db_Table_Rowset|null
+	 */
+	public function findCatalogRowsetForUser(Zend_Controller_Request_Abstract $rq) {
+		require_once 'catalog/models/Catalog.php';
+		$catalog = new Catalog();
+
+		$select = $catalog->selectForRowOwner($rq);
+
+		try {
+			return $catalog->fetchAll($select);
+		} catch (Zend_Db_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString(), Zend_Log::ERR);
+			return null;
+		}
+	}
+
+	/**
+	 * @param $id
+	 * @param $rq
+	 * @return Zend_Db_Table_Row|null
+	 */
+	public function findCatalogRowForUser($id, Zend_Controller_Request_Abstract $rq) {
+		require_once 'catalog/models/Catalog.php';
+		$catalog = new Catalog();
+
+		$select = $catalog->selectForRowOwner($rq);
+		$select->where('id = ?', $id, Zend_Db::INT_TYPE);
+
+		try {
+			return $catalog->fetchRow($select);
+		} catch (Zend_Db_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString(), Zend_Log::ERR);
+			return null;
+		}
+	}
+
+	/**
+	 * @param $id
+	 * @return array
+	 */
+	public function findServicesRowsetForCatalogId($id) {
+		require_once 'catalog/models/CatalogServiceCost.php';
+		$serviceCost = new CatalogServiceCost();
+		
+		$selectCost = $serviceCost->select()
+			->where('catalog_id = ?', $id, Zend_Db::INT_TYPE);
+
+		try {
+			$rowsetCost = $serviceCost->fetchAll($selectCost);
+		} catch (Zend_Db_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString());
+			return false;
+		}
+		
+
+		require_once 'catalog/models/CatalogService.php';
+		$service = new CatalogService();
+		
+		try {
+			$rowsetService = $service->fetchAll();
+		} catch (Zend_Db_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString(), Zend_Log::ERR);
+			return false;
+		}
+
+		$result = array();
+		foreach ($rowsetService as $service) {
+			$data = $service->toArray();
+			
+			if (null !== ($cost = $this->_hasServiceCost($service, $rowsetCost))) {
+				$data['cost_min'] = $cost->cost_min;
+				$data['cost_max'] = $cost->cost_max;
+			} else {
+				$data['cost_min'] = null;
+				$data['cost_max'] = null;
+			}
+
+			$result[] = $data;
+		}
+		
+		return $result;
+	}
+	
+	/**
+	 * @param $service
+	 * @param $rowsetCost
+	 * @return Zend_Db_Table_Row_Abstract|null
+	 */
+	private function _hasServiceCost(Zend_Db_Table_Row_Abstract $service, Zend_Db_Table_Rowset_Abstract $rowsetCost) {
+		foreach ($rowsetCost as $cost) {
+			if ($cost->catalog_service_id == $service->id) {
+				return $cost;
+			}
+		}
+		
+		return null;
+	}
+	
+	public function saveServicesCost($catalogId, Zend_Controller_Request_Abstract $rq) {
+		require_once 'catalog/models/CatalogServiceCost.php';
+		$serviceCost = new CatalogServiceCost();
+
+		require_once 'Zend/Filter/Digits.php';
+		$filter = new Zend_Filter_Int();
+		
+		$catalogId = $filter->filter($catalogId);
+		
+		try {
+			foreach ((array) $rq->getPost('data') as $catalogServicId => $data) {
+				$catalogServicId = $filter->filter($catalogServicId);
+
+				$serviceCost
+					->delete("catalog_id = '$catalogId' AND catalog_service_id = '$catalogServicId'");
+					
+				$data = array_filter($data);
+				$data = array_intersect_key(
+					$data, array_flip(array('cost_min','cost_max')));
+
+				if ((!isset($data['cost_min']) || empty($data['cost_min']))
+						&& (!isset($data['cost_max']) || empty($data['cost_max']))) {
+					continue;
+				}
+					
+				$data['cost_min'] = (float) $data['cost_min'];
+				$data['cost_max'] = (float) $data['cost_max'];
+				$data['catalog_id'] = $catalogId;
+				$data['catalog_service_id'] = $catalogServicId;
+
+				$serviceCost->insert($data);
+			}
+			return true;
+		} catch (Zend_Db_Exception $e) {
+			Zend_Registry::get('logger')
+				->log($e->getMessage() ."\n".$e->getTraceAsString(), Zend_Log::ERR);
+			return false;
+		}			
+	}
+}
