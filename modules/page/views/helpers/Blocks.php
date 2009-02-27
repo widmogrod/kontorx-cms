401fef28b3419ff4eaddf53f1dd6f899057492d7
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2009-02-27 13:45:20 +0000

    Update for elco~

--- modules/page/views/helpers/Blocks.php
+++ modules/page/views/helpers/Blocks.php
@@ -2,18 +2,21 @@
 require_once 'Zend/View/Helper/Abstract.php';
 class Page_View_Helper_Blocks extends Zend_View_Helper_Abstract {
 	protected $_blocks = array();
-
+        protected $_blockName = null;
 	/**
 	 * Konstruktor
 	 *
 	 * @param Zend_Db_Table_Rowset_Abstract|object|null $blocks
-	 * @param string|null $url
+	 * @param string|null $blockName
 	 * @return Page_View_Helper_Blocks|string
 	 */
-	public function blocks($blocks = null, $url = null) {
+	public function blocks($blocks = null, $blockName = null) {
 		if (null === $blocks) {
 			$blocks = $this->view->pageBlocks;
 		}
+                
+                $this->_blockName = (string) $blockName;
+
 		if (empty($blocks)) {
 			print '<!-- Page_View_Helper_Blocks::blocks $blocks is empty or is not object-->';
 			return $this;
@@ -22,8 +25,15 @@ class Page_View_Helper_Blocks extends Zend_View_Helper_Abstract {
 		foreach ($blocks as $block) {
 			$this->_blocks[$block->block_name] = $block->content;
 		}
-		return (null === $url) ? $this : $this->{$url};
+
+		return $this;
 	}
+
+        public function  __toString() {
+            return array_key_exists($this->_blockName, $this->_blocks)
+                ? $this->_blocks[$this->_blockName]
+                : ('<div class="block">' . implode('</div><div class="block">', $this->_blocks) . '</div>');
+        }
 
 	/**
 	 * Isset
