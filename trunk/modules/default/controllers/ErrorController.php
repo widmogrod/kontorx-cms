89f4c65adeb6be62787fab3c1cfee277d5f17dab
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2009-03-06 04:47:39 +0000

    update

--- trunk/modules/default/controllers/ErrorController.php
+++ trunk/modules/default/controllers/ErrorController.php
@@ -1,51 +1,50 @@
 <?php
 require_once 'KontorX/Controller/Action.php';
 class Default_ErrorController extends KontorX_Controller_Action {
-	public $skin = array(
-		'layout' => 'index'
-	);
+    public $skin = array(
+        'layout' => 'index'
+    );
 
     /**
      * Przechywytywanie błędów
-     *
      */
     public function errorAction() {
-		$errors = $this->_getParam('error_handler');
-		$exception = $errors->exception;
-		switch ($errors->type) {
-			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
-				$this->_helper->viewRenderer->render('error.nocontroller');
-				break;
-			case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
-				$this->_helper->viewRenderer->render('error.noaction');
-				break;
+        $errors = $this->_getParam('error_handler');
+        $exception = $errors->exception;
 
+        switch ($errors->type) {
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_CONTROLLER:
+                $this->_helper->viewRenderer->render('error.nocontroller');
+                break;
+            case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_NO_ACTION:
+                $this->_helper->viewRenderer->render('error.noaction');
+                break;
             case Zend_Controller_Plugin_ErrorHandler::EXCEPTION_OTHER:
-            	$this->view->message = $exception->getMessage();
-            	$this->view->trace = $exception->getTrace();
-				break;
-
-			default:
-				$this->view->message = $exception->getMessage();
-            	$this->view->trace = $exception->getTrace();
-				break;
+                $this->view->message = $exception->getMessage();
+                $this->view->trace = $exception->getTrace();
+                break;
+            default:
+                $this->view->message = $exception->getMessage();
+                $this->view->trace = $exception->getTrace();
+                break;
         }
 
         require_once 'KontorX/Util/Functions.php';
-    	$ip = KontorX_Util_Functions::getIP();
+        $ip = KontorX_Util_Functions::getIP();
 
-		Zend_Registry::get('loggerFramework')
-			->log("$ip :: " . get_class($exception) . " " . $exception->getMessage() . "\n" .  $exception->getTraceAsString(), Zend_Log::CRIT);
+        Zend_Registry::get('loggerFramework')
+        ->log("$ip :: " . get_class($exception) . " " . $exception->getMessage() . "\n" .  $exception->getTraceAsString(), Zend_Log::CRIT);
     }
 
-    /**
-     * Użytkownik jest zalogowany ale ma niewystarczające uprawnienia!
-     */
+        /**
+         * Użytkownik jest zalogowany ale ma niewystarczające uprawnienia!
+         */
     public function privilegesAction() {
-    	require_once 'KontorX/Util/Functions.php';
-    	$ip = KontorX_Util_Functions::getIP();
-    	Zend_Registry::get('loggerFramework')
-			->log("$ip :: " . __FUNCTION__, Zend_Log::CRIT);
+        require_once 'KontorX/Util/Functions.php';
+        $ip = KontorX_Util_Functions::getIP();
+
+        Zend_Registry::get('loggerFramework')
+        ->log("$ip :: " . __FUNCTION__, Zend_Log::CRIT);
     }
 }
 
