d951df71cf16d5bf046d612bc2fc60285d1a9ef4
widmogrod <widmogrod@fff415ae-1556-0410-a898-a1118da6f6b6>
2009-03-17 12:41:26 +0000

    update Gatalog and dataGrid

--- trunk/modules/catalog/controllers/IndexController.php
+++ trunk/modules/catalog/controllers/IndexController.php
@@ -14,11 +14,11 @@ class Catalog_IndexController extends KontorX_Controller_Action {
         )
     );
 
-    	public $cache = array(
-//    		'index' => array('id' => 'params'),
-//    		'az' => array('id' => array('param' => 'string')),
-//    		'show' => array('id' => array('param' => 'id'))
-    	);
+    public $cache = array(
+//            		'index' => array('id' => 'params'),
+        //    		'az' => array('id' => array('param' => 'string')),
+        //    		'show' => array('id' => array('param' => 'id'))
+    );
 
     public $contexts = array(
         'mapdata' => array('json'),
@@ -40,59 +40,117 @@ class Catalog_IndexController extends KontorX_Controller_Action {
         ->setAutoJsonSerialization(false)
         ->initContext();
 
-        $this->view->addHelperPath('KontorX/View/Helper');
+        $configMain = $this->_helper->loader->config('config.ini');
+        $this->view->apiKey = $configMain->gmap->{BOOTSTRAP}->apiKey;
     }
 
+    /**
+     * Strona główna
+     * @todo dodać ograniczenia programowe dla rekordow promo itd.
+     * @todo cache paginacji
+     */
     public function indexAction() {
         $config = $this->_helper->loader->config('index.xml');
 
-        $configMain = $this->_helper->loader->config('config.ini');
-        $this->view->apiKey = $configMain->gmap-> {BOOTSTRAP}->apiKey;
+        require_once 'catalog/models/Catalog.php';
+        $catalog = new Catalog();
+
+        /**
+         * Set cache for @see KontorX_DataGrid_Adapter_Abstract
+         * and for @see Zend_Paginator
+         */
+        $cache = Zend_Registry::get('cacheDBQuery');
+        KontorX_DataGrid_Adapter_Abstract::setCache($cache);
+        Zend_Paginator::setCache($cache);
+
+        // rekordy promocyjne
+        $select = $catalog->selectForListPromoPlus();
+        $gridPromo = KontorX_DataGrid::factory($select, $config->dataGrid);
+        $this->view->gridPromo = $gridPromo;
+
+        // rekordy zwykle
+        $select = $catalog->selectForListDefault();
+        $grid = KontorX_DataGrid::factory($select, $config->dataGrid);
+        $this->view->grid = $grid;
+
+        $page = $this->_getParam('page');
+        $onPage = 15;
+
+        $paginator = Zend_Paginator::factory($select);        
+        $grid->setPaginator($paginator);
+        $grid->setPagination($page, $onPage);
+    }
+
+    /**
+     * Strona wyswietlania kategorii
+     * @todo dodać ograniczenia programowe dla rekordow promo itd.
+     * @todo cache paginacji
+     */
+    public function categoryAction() {
+        $config = $this->_helper->loader->config('index.xml');
+
+        $categoryUrl = $this->_getParam('url', $config->default->category->url);
+        $this->view->categoryUrl = $categoryUrl;
+
+        require_once 'catalog/models/CatalogDistrict.php';
+        $catalogDistrict = new CatalogDistrict();
+
+        // sprawdzanie czy istnieje kategoria
+        $select = $catalogDistrict->select();
+        $select
+        ->where('url = ?', $categoryUrl);;
+
+        try {
+            $row = $catalogDistrict->fetchRow($select);
+        } catch (Zend_Db_Table_Abstract $e) {
+            // logowanie wyjatku
+            $logger = Zend_Registry::get('logger');
+            $logger->log($e->getMessage() . "\n" . $e->getTraceAsString(), Zend_Log::ERR);
+
+            $row = null;
+        }
+
+        // czy aby napewno ..
+        if (!$row instanceof KontorX_Db_Table_Tree_Row_Abstract) {
+            $this->_helper->viewRenderer->render('category.no.exsists');
+            return;
+        }
+
+        $this->view->categoryRow = $row;
 
         require_once 'catalog/models/Catalog.php';
         $catalog = new Catalog();
-        $select = $catalog->selectForListPromoPlus();
-//        $stmt = $select->query();
-//        $stmt->fetchAll();
 
+        /**
+         * Set cache for @see KontorX_DataGrid_Adapter_Abstract
+         * and for @see Zend_Paginator
+         */
+        $cache = Zend_Registry::get('cacheDBQuery');
+        KontorX_DataGrid_Adapter_Abstract::setCache($cache);
+        Zend_Paginator::setCache($cache);
 
-//        $stmt = $select->query();
-//        $rowset = $stmt->fetchAll();
-
+        // rekordy promocyjne
+        $select = $catalog->selectForListPromoPlus($row);
         $gridPromo = KontorX_DataGrid::factory($select, $config->dataGrid);
-//        $gridPromo->setColumns($config->dataGridColumns->toArray());
-//        $gridPromo->setValues((array) $this->_getParam('filter'));
         $this->view->gridPromo = $gridPromo;
 
-        // ..
-
-        $select = $catalog->selectForListDefault();
-
+        // rekordy zwykle
+        $select = $catalog->selectForListDefault($row);
         $grid = KontorX_DataGrid::factory($select, $config->dataGrid);
-//        $grid->setColumns($config->dataGridColumns->toArray());
-//        $grid->setValues((array) $this->_getParam('filter'));
+        $this->view->grid = $grid;
 
-        //		// setup grid paginatior
-        //		$select = $grid->getAdapter()->getSelect();
-        //
-        // inicjowanie alfabetycznego sortowania
-        //		$this->_initAlphabetical($select);
+        $page = $this->_getParam('page');
+        $onPage = 15;
 
         $paginator = Zend_Paginator::factory($select);
         $grid->setPaginator($paginator);
-        $grid->setPagination($this->_getParam('page'), 15);
-
-        $this->view->grid = $grid;
+        $grid->setPagination($page, $onPage);
     }
 
-    public function addAction() {
-
-    }
-
-    public function updateAction() {
-
-    }
-
+    /**
+     * Feedback o błędzie w wizytówce
+     * @todo stworzyć by był bardziej akcja globalna nie modulową
+     */
     public function errorAction() {
         $config = $this->_helper->loader->config('index.xml');
 
@@ -140,14 +198,18 @@ class Catalog_IndexController extends KontorX_Controller_Action {
             $referer = $this->_getParam('id');
         }
         return array(
-                        'referer' => $referer,
-                        'message' => "Zostały znalezione następujące błędy:\n".
-                                                 " 1. \n".
-                                                 " 2. \n".
-                                                 " 3. \n"
+            'referer' => $referer,
+            'message' => "Zostały znalezione następujące błędy:\n".
+                         " 1. \n".
+                         " 2. \n".
+                         " 3. \n"
         );
     }
 
+    /**
+     * Dane dla mapy OFF
+     */
+    /*
     public function mapdataAction() {
         require_once 'catalog/models/Catalog.php';
         $catalog = new Catalog();
@@ -167,9 +229,6 @@ class Catalog_IndexController extends KontorX_Controller_Action {
     }
 
     public function mapAction() {
-        $config = $this->_helper->loader->config('config.ini');
-        $this->view->apiKey = $config->gmap-> {BOOTSTRAP}->apiKey;
-
         $id = $this->view->id = (int) $this->_getParam('id');
         if ($id > 0) {
             require_once 'catalog/models/Catalog.php';
@@ -183,201 +242,6 @@ class Catalog_IndexController extends KontorX_Controller_Action {
             }
         }
     }
-
-    public function showAction() {
-        $primaryId = $this->_getParam('id');
-        require_once 'Zend/Filter.php';
-        $primaryId = Zend_Filter::get($primaryId, 'Int');
-
-        if (null === $primaryId) {
-            $this->_helper->viewRenderer->render('show.error');
-            return;
-        }
-
-        if ($this->_hasParam('_site')) {
-            $this->_helper->system->layout('catalog_site');
-        }
-
-        $config = $this->_helper->loader->config('config.ini');
-        $this->view->apiKey = $config->gmap-> {BOOTSTRAP}->apiKey;
-
-        require_once 'catalog/models/Catalog.php';
-        $catalog = new Catalog();
-
-        try {
-            $select = $catalog->select()
-            ->where('id = ?', $primaryId, Zend_Db::INT_TYPE);
-            $catalogRow = $catalog->fetchRow($select);
-        } catch (Zend_Db_Table_Abstract $e) {
-            $this->_helper->viewRenderer->render('show.error');
-            return;
-        }
-
-        if (!$catalogRow instanceof Zend_Db_Table_Row_Abstract) {
-            $this->_helper->viewRenderer->render('show.error');
-            return;
-        }
-
-        $this->view->catalogRow = $catalogRow;
-        // ustawiam parametr dla helpera
-        $this->view->categoryTree()->setActiveId($catalogRow->catalog_district_id);
-
-        //		$districtRow = $catalogRow->findParentRow('CatalogDistrict');
-        //		if ($districtRow instanceof KontorX_Db_Table_Tree_Row_Abstract) {
-        //			$this->view->districtRowset = $districtRow->findDescendant(null, true);
-        //		}
-
-        $tab = strtolower($this->_getParam('tab','ogolne','personel'));
-        $this->view->tab = $tab;
-        switch ($tab) {
-            default:
-            case 'ogolne':;
-                $this->view->optionsRowset = $catalogRow->findManyToManyRowset('CatalogOptions','CatalogHasCatalogOptions');
-                $this->view->timeRow = $catalogRow->findDependentRowset('CatalogTime')->current();
-                break;
-            case 'uslugi':
-                $this->_setupModelCatalogService();
-                $this->view->serviceRowset = $catalogRow->findManyToManyRowset('CatalogService','CatalogServiceCost');
-                break;
-            case 'personel':
-                $this->_setupModelCatalogService();
-                $this->view->staffRowset = $catalogRow->findDependentRowset('CatalogStaff');
-                break;
-        }
-
-        /*$this->_setupModelCatalogType();
-        $this->view->typeRow = $catalogRow->findParentRow('CatalogType');*/
-
-        $this->_setupModelCatalogImage();
-        $this->view->imagesRowset = $catalogRow->findDependentRowset('CatalogImage');
-    }
-
-    public function categoryAction() {
-        $config = $this->_helper->loader->config('index.xml');
-
-        $configMain = $this->_helper->loader->config('config.ini');
-        $this->view->apiKey = $configMain->gmap-> {BOOTSTRAP}->apiKey;
-
-        $categoryUrl = $this->_getParam('url', $config->default->category->url);
-        $this->view->categoryUrl = $categoryUrl;
-
-        require_once 'catalog/models/CatalogDistrict.php';
-        $catalogDistrict = new CatalogDistrict();
-
-        // sprawdzanie czy istnieje kategoria
-        $select = $catalogDistrict->select();
-        $select
-        ->where('url = ?', $categoryUrl);;
-
-        try {
-            $row = $catalogDistrict->fetchRow($select);
-        } catch (Zend_Db_Table_Abstract $e) {
-            // logowanie wyjatku
-            $logger = Zend_Registry::get('logger');
-            $logger->log($e->getMessage() . "\n" . $e->getTraceAsString(), Zend_Log::ERR);
-
-            $row = null;
-        }
-
-        // czy aby napewno ..
-        if (!$row instanceof KontorX_Db_Table_Tree_Row_Abstract) {
-            $this->_helper->viewRenderer->render('category.no.exsists');
-            return;
-        }
-
-        $this->view->categoryRow = $row;
-
-        require_once 'catalog/models/Catalog.php';
-        $catalog = new Catalog();
-        $select = $catalog->selectForListPromoPlus($row);
-
-        $gridPromo = KontorX_DataGrid::factory($select, $config->dataGrid);
-//        $gridPromo->setColumns($config->dataGridColumns->toArray());
-//        $gridPromo->setValues((array) $this->_getParam('filter'));
-        $this->view->gridPromo = $gridPromo;
-
-        // ..
-
-        $select = $catalog->selectForListDefault($row);
-
-        $grid = KontorX_DataGrid::factory($select, $config->dataGrid);
-//        $grid->setColumns($config->dataGridColumns->toArray());
-//        $grid->setValues((array) $this->_getParam('filter'));
-
-        //		$grid = KontorX_DataGrid::factory($select);
-
-        // inicjowanie alfabetycznego sortowania
-        //		$this->_initAlphabetical($select);
-
-        //		$grid->setColumns($config->dataGridColumns->toArray());
-        //		$grid->setValues((array) $this->_getParam('filter'));
-
-        // setup grid paginatior
-        $paginator = Zend_Paginator::factory($select);
-        $grid->setPaginator($paginator);
-        $grid->setPagination($this->_getParam('page'), 15);
-
-        $this->view->grid = $grid;
-    }
-
-    /**
-     * Wyszukiwarka
-     *
-     * @return void
-     */
-    public function searchAction() {
-        $config = $this->_helper->loader->config('index.xml');
-
-        $configMain = $this->_helper->loader->config('config.ini');
-        $this->view->apiKey = $configMain->gmap-> {BOOTSTRAP}->apiKey;
-
-        $rq = $this->getRequest();
-
-        // filtrowanie
-        $validators = array();
-        $filters = array(
-    '*' => array(
-                new Zend_Filter_StripTags(),
-                new KontorX_Filter_MagicQuotes()),
-            // 'name'  => new KontorX_Filter_SearchText(),
-    'name' => 'StringTrim',
-    'district' => array('Digits'),
-            // 'options' => new KontorX_Filter_Array(),
-    'week' => 'Digits'
-        );
-        //    $input = new Zend_Filter_Input($filters, $validators);
-
-        $data = $rq->getParams();
-
-        $f = new KontorX_Filter_MagicQuotes();
-        $data = $f->filter($data);
-        //    $input->setData($rq->getPost());
-        $this->view->input = $data;
-
-        $district = new CatalogDistrict();
-        $this->view->districtRowset = $district->fetchAllCache();
-
-        $options = new CatalogOptions();
-        $this->view->optionsArray = $options->fetchAllOptionsArrayCache();
-
-        $service = new CatalogService();
-        $this->view->serviceArray = $service->fetchAllOptionsArrayCache();
-
-        $catalog = new Catalog();
-        $select = $catalog->selectForSearch($data);
-
-        $grid = KontorX_DataGrid::factory($select, $config->dataGrid);
-//        $grid->setColumns($config->dataGridColumns->toArray());
-//        $grid->setValues((array) $this->_getParam('filter'));
-
-        // setup grid paginatior
-        $paginator = Zend_Paginator::factory($select);
-        $grid->setPaginator($paginator);
-        $grid->setPagination($this->_getParam('page'), 15);
-
-        $this->view->grid = $grid;
-    }
-
     public function nearAction() {
         // wylanczamy layout
         $this->_helper->layout->disableLayout();
@@ -417,26 +281,139 @@ class Catalog_IndexController extends KontorX_Controller_Action {
             ->log($e->getMessage() . "\n" . $e->getTraceAsString(), Zend_Log::ERR);
         }
     }
+     */
 
-    private function _initAlphabetical(Zend_Db_Select $select) {
-        $this->view->az = $az = array(
-                    'A','B','C','Ć','D','E','F','G','H',
-                    'I','J','K','L','Ł','M','N','Ń','O',
-                    'P','R','S','Ś','T','U','W','Y','Z',
-                    'Ź','Ż');
+    /**
+     * Pokaz prezentacje rekordu
+     * @todo dodać cache!
+     */
+    public function showAction() {
+        $primaryId = $this->_getParam('id');
+        require_once 'Zend/Filter.php';
+        $primaryId = Zend_Filter::get($primaryId, 'Int');
 
-        $string = $this->_getParam('string');
-        $string = strtoupper($string);
-
-        $this->view->string = $string;
-
-        if (in_array($string, $az)) {
-            // setup select
-            $select
-            ->where('name LIKE ?', "$string%")
-            ->order('name ASC');
+        if (null === $primaryId) {
+            $this->_helper->viewRenderer->render('show.error');
+            return;
         }
+
+        if ($this->_hasParam('_site')) {
+            $this->_helper->system->layout('catalog_site');
+        }
+
+        require_once 'catalog/models/Catalog.php';
+        $catalog = new Catalog();
+
+        try {
+            $select = $catalog->select()
+            ->where('id = ?', $primaryId, Zend_Db::INT_TYPE);
+            $catalogRow = $catalog->fetchRow($select);
+        } catch (Zend_Db_Table_Abstract $e) {
+            $this->_helper->viewRenderer->render('show.error');
+            return;
+        }
+
+        if (!$catalogRow instanceof Zend_Db_Table_Row_Abstract) {
+            $this->_helper->viewRenderer->render('show.error');
+            return;
+        }
+
+        $this->view->catalogRow = $catalogRow;
+        // ustawiam parametr dla helpera
+        $this->view->categoryTree()->setActiveId($catalogRow->catalog_district_id);
+
+        $tab = strtolower($this->_getParam('tab','ogolne','personel'));
+        $this->view->tab = $tab;
+        switch ($tab) {
+            default:
+                case 'ogolne':;
+                    $this->view->optionsRowset = $catalogRow->findManyToManyRowset('CatalogOptions','CatalogHasCatalogOptions');
+                    $this->view->timeRow = $catalogRow->findDependentRowset('CatalogTime')->current();
+                    break;
+                case 'uslugi':
+                    $this->_setupModelCatalogService();
+                    $this->view->serviceRowset = $catalogRow->findManyToManyRowset('CatalogService','CatalogServiceCost');
+                    break;
+                case 'personel':
+                    $this->_setupModelCatalogService();
+                    $this->view->staffRowset = $catalogRow->findDependentRowset('CatalogStaff');
+                    break;
+        }
+
+        $this->_setupModelCatalogImage();
+        $this->view->imagesRowset = $catalogRow->findDependentRowset('CatalogImage');
     }
+
+    
+
+    /**
+     * Wyszukiwarka
+     * @todo dodac cache! - np. catalog_district i sprawdzanie czy istnieje .. czyszczone gdy zostanie dodana nowa dzielnica
+     * @todo optymalizować
+     */
+    public function searchAction() {
+        $config = $this->_helper->loader->config('index.xml');
+
+        $rq = $this->getRequest();
+        $data = $rq->getParams();
+
+        $f = new KontorX_Filter_MagicQuotes();
+        $data = $f->filter($data);
+        $this->view->input = $data;
+
+        $district = new CatalogDistrict();
+        $this->view->districtRowset = $district->fetchAllCache();
+
+        $options = new CatalogOptions();
+        $this->view->optionsArray = $options->fetchAllOptionsArrayCache();
+
+        $service = new CatalogService();
+        $this->view->serviceArray = $service->fetchAllOptionsArrayCache();
+
+        $catalog = new Catalog();
+
+        /**
+         * Set cache for @see KontorX_DataGrid_Adapter_Abstract
+         * and for @see Zend_Paginator
+         */
+        $cache = Zend_Registry::get('cacheDBQuery');
+        KontorX_DataGrid_Adapter_Abstract::setCache($cache);
+        Zend_Paginator::setCache($cache);
+
+        // rekordy all
+        $select = $catalog->selectForSearch($data);
+        $grid = KontorX_DataGrid::factory($select, $config->dataGrid);
+        $this->view->grid = $grid;
+
+        $page = $this->_getParam('page');
+        $onPage = 15;
+
+        $paginator = Zend_Paginator::factory($select);
+        $grid->setPaginator($paginator);
+        $grid->setPagination($page, $onPage);
+    }
+
+    
+
+//    private function _initAlphabetical(Zend_Db_Select $select) {
+//        $this->view->az = $az = array(
+//                'A','B','C','Ć','D','E','F','G','H',
+//                'I','J','K','L','Ł','M','N','Ń','O',
+//                'P','R','S','Ś','T','U','W','Y','Z',
+//                'Ź','Ż');
+//
+//        $string = $this->_getParam('string');
+//        $string = strtoupper($string);
+//
+//        $this->view->string = $string;
+//
+//        if (in_array($string, $az)) {
+//            // setup select
+//            $select
+//            ->where('name LIKE ?', "$string%")
+//            ->order('name ASC');
+//        }
+//    }
 
     private function _setupModelCatalogType() {
         $config = $this->_helper->loader->config();
